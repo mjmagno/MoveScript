6c54881db33f57b52489f105cc1ee923e49795c3
MiguelMagno <mjmagno@umich.edu>
2024-01-27 13:25:45 -0500

    Changed custom sound library to playsound, added concurrency mechanics to alarm

--- CustomSound.py
+++ CustomSound.py
@@ -1,15 +1,4 @@
-import pygame
+from playsound import playsound
 
 def play_custom_sound(sound_file_path):
-    pygame.init()
-    pygame.mixer.init()
-
-    try:
-        sound = pygame.mixer.Sound(sound_file_path)
-        sound.play()
-        pygame.time.delay(int(sound.get_length() * 1000))  # Delay to allow sound to finish playing
-    except Exception as e:
-        print(f"Error playing sound: {e}")
-    finally:
-        pygame.mixer.quit()
-        pygame.quit()
+   playsound(sound_file_path)
--- GetMoving.py
+++ GetMoving.py
@@ -1,6 +1,24 @@
 import datetime
 import time
+import threading
+import os
+
 from CustomSound import play_custom_sound
+
+def alarm(sound_file_path):
+    t = threading.current_thread()
+    while (getattr(t,"do_run",True)):
+        # Need to listen, not sure if the sound is overlapping 
+        play_custom_sound(sound_file_path)
+
+# Accepts a time in second and then puts program to sleep for that long 
+def sleep (timer) :
+    for i in range(0, timer):
+        time.sleep(1)
+
+
+
+
 
 
 print("Enter a mode (Gaming or Coding)")
@@ -8,21 +26,19 @@ print("Enter a mode (Gaming or Coding)")
 #If mode is gaming, set a constant 30 minute timer 
 
 
-
-mode = input(">> ")
-if (mode == 'Gaming'):
-    for i in range(0, 1800):
-        time.sleep(1)
-elif (mode == 'Coding'):
+t1 = threading.Thread(target=alarm,args=("WhoseGonnaCarryTheBoats.wav",))
+mode = (input(">> ")).lower()
+if (mode == 'gaming'):
+    sleep(1800)
+elif (mode == 'coding'):
     print("Enter the time you'd like to take for this problem")
     minutes = input(">> ")
     seconds = int(minutes) * 60
-    for i in range(0, seconds):
-        time.sleep(1)
-userInput = ''
-while (userInput != 'S'):
-    # userInput = input("Input S to stop: ")
+    sleep(seconds)
+t1.start()
+while (input("Enter S to stop ").lower() == 'S'):
+    t1.do_run = True
     # Make noise until user input 
-    play_custom_sound("WhoseGonnaCarryTheBoats.wav")
+t1.do_run = False
 
 
