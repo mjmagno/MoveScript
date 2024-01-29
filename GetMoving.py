141e2901777bfe5150ecd83b56c352aa2c78bba0
MiguelMagno <mjmagno@umich.edu>
2024-01-29 16:11:57 -0500

    Added a loop to the main body, alarm now outputs how many minutes remaining

--- GetMoving.py
+++ GetMoving.py
@@ -8,37 +8,48 @@ from CustomSound import play_custom_sound
 def alarm(sound_file_path):
     t = threading.current_thread()
     while (getattr(t,"do_run",True)):
-        # Need to listen, not sure if the sound is overlapping 
         play_custom_sound(sound_file_path)
 
 # Accepts a time in second and then puts program to sleep for that long 
+#TODO "1.0 minutes remaining"
+#TODO remove ".0" from minutes remaining 
 def sleep (timer) :
     for i in range(0, timer):
+        if (i % 60 == 0):
+            print(str(timer / 60 - i / 60) + " minutes remaining")
         time.sleep(1)
 
 
+if __name__ == "__main__":
+    t1 = threading.Thread(target=alarm,args=("WhoseGonnaCarryTheBoats.wav",))
+    print("Enter a mode (Gaming or Coding)")
+    #If mode is coding, then set a variable time for each alarm     
+    #If mode is gaming, set a constant 30 minute timer 
+
+    mode = (input(">> ")).lower()
+    userInput = ""
+    minutes = 0
+    if (mode == 'gaming'):
+        minutes = 30
+    while (userInput != 'e'):
+        if (mode == 'coding'):
+            print("Enter the time you'd like to take for this problem")
+            minutes = input(">> ")
+        seconds = int(minutes) * 60
+        sleep(seconds)
+        t1.start()
+        # Not sure if I like how this is implented, a while seems weird but using an if statement
+        # will result in the program infinite looping if the user misinputs
+        while (input("Enter S to stop ").lower() == 'S'):
+            t1.do_run = True
+        t1.do_run = False
+        print("Enter E to exit or press another key to continue")
+        userInput = input(">> ").lower()
 
 
-
-
-print("Enter a mode (Gaming or Coding)")
-#If mode is coding, then set a variable time for each alarm     
-#If mode is gaming, set a constant 30 minute timer 
-
-
-t1 = threading.Thread(target=alarm,args=("WhoseGonnaCarryTheBoats.wav",))
-mode = (input(">> ")).lower()
-if (mode == 'gaming'):
-    sleep(1800)
-elif (mode == 'coding'):
-    print("Enter the time you'd like to take for this problem")
-    minutes = input(">> ")
-    seconds = int(minutes) * 60
-    sleep(seconds)
-t1.start()
-while (input("Enter S to stop ").lower() == 'S'):
-    t1.do_run = True
-    # Make noise until user input 
-t1.do_run = False
-
-
+#TODO
+# Add a way to pause timer
+# Make input printing consistent 
+# Convert time remaining to int 
+# Error handling
+# Different alarms 
